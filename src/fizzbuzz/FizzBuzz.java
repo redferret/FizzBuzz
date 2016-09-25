02fe979a4ec9a39b77985fd95d81b1b78833e5c6
Richard DeSilvey <rdesilvey@gmail.com>
2016-09-25 16:07:42 -0600

    Added Unit test and method for the game

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -8,10 +8,17 @@ package fizzbuzz;
 public class FizzBuzz {
 
     /**
-     * @param args the command line arguments
+     * Constructs the output array of Strings based on the classic FizzBuzz 
+     * game. If the number is divisible by 3 the output will be 'Fizz', 
+     * if divisible by 5, print "Buzz", if divisible by both 3 and 5 print
+     * 'Fizz Buzz' otherwise print the number.
+     * @param start The number to start the game at
+     * @param finish The number to finish the game at
+     * @return The strings for each number from 'start' to 'finish'. The
+     * length of the array is (finish - start).
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String[] fizzBuzz(int start, int finish){
+        return null;
     }
     
 }
---
+++ test/FizzBuzzTest.java
@@ -0,0 +1,46 @@
+import fizzbuzz.FizzBuzz;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Richard
+ */
+public class FizzBuzzTest {
+    
+    @Test
+    public void fizzBuzzMethodTest(){
+        
+        int start = 0;
+        int finish = 20;
+        
+        String[] output = FizzBuzz.fizzBuzz(start, finish);
+        
+        assertNotNull("The output is null", output);
+        
+        Integer expectedLength = finish - start;
+        Integer actualLength = output.length;
+        assertEquals("Lengths don't match", expectedLength, actualLength);
+        
+        String expectedStr = "Fizz";
+        String actualStr = output[3];
+        assertEquals("Output is incorrect", expectedStr, actualStr);
+        
+        expectedStr = "Fizz";
+        actualStr = output[3];
+        assertEquals("Output is incorrect", expectedStr, actualStr);
+        
+        expectedStr = "Buzz";
+        actualStr = output[5];
+        assertEquals("Output is incorrect", expectedStr, actualStr);
+        
+        expectedStr = "Fizz Buzz";
+        actualStr = output[15];
+        assertEquals("Output is incorrect", expectedStr, actualStr);
+        
+        expectedStr = "1";
+        actualStr = output[1];
+        assertEquals("Output is incorrect", expectedStr, actualStr);
+    }
+    
+}
